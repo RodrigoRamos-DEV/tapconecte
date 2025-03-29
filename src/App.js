0cdc1557cc60185468947009955ef0840f89ef87
Rodrigo Ramos <rodrigomramos18@gmail.com>
2025-03-28 23:41:00 -0300

    vcorreta

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-// src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Pages/Login';
-import AdminPanel from './Pages/AdminPanel';
-import Register from './Pages/Register';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
 import CardForm from './Pages/CardForm';
 import CardPreview from './Pages/CardPreview';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
+  const [cardData, setCardData] = useState(null);
+
+  const handleCardSubmit = (data) => {
+    setCardData(data);
+  };
+
   return (
     <Router>
-      <Routes>
-        {/* Rota de login */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* Rota do painel administrativo */}
-        <Route path="/admin-panel" element={<AdminPanel />} />
-        
-        {/* Rota de registro */}
-        <Route path="/register" element={<Register />} />
-        
-        {/* Rota do formulário do card */}
-        <Route path="/form" element={<CardForm />} />
-        <Route path="/card-preview" element={<CardPreview />} />
-
-     
-        
-        {/* Página inicial: redireciona para o login */}
-        <Route path="/" element={<Login />} /> {/* Default route */}
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<CardForm onSubmit={handleCardSubmit} />} />
+          <Route path="/preview" element={cardData ? <CardPreview cardData={cardData} /> : <Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/Pages/CardForm.js
+++ src/Pages/CardForm.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useNavigate } from 'react-router-dom';
 import { ChromePicker } from 'react-color';
 import logoImage from '../assets/images/logo.jpg';
 import './cardform.css';
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "./firebase";
 
 const backgroundStyle = {
   backgroundImage: `url(${logoImage})`,
@@ -17,7 +16,7 @@ const backgroundStyle = {
   alignItems: 'center',
 };
 
-const CardForm = ({ user, onSubmit }) => {
+const CardForm = ({ onSubmit }) => {
   const [activeTab, setActiveTab] = useState('profile');
   const [name, setName] = useState('');
   const [instagram, setInstagram] = useState('');
@@ -41,43 +40,20 @@ const CardForm = ({ user, onSubmit }) => {
   const [error, setError] = useState('');
   const [showPicker, setShowPicker] = useState({ bg: false, label: false, font: false });
   const [currentPicker, setCurrentPicker] = useState(null);
-  const [selectedIcon] = useState(null); // Estado para armazenar o ícone selecionado
+  const [selectedIcon, setSelectedIcon] = useState(null); // Estado para armazenar o ícone selecionado
   const pickerRef = useRef(null);
   const navigate = useNavigate();
   const [logoUploaded, setLogoUploaded] = useState(false);
-  const [bgImageUploaded, setBgImageUploaded] = useState(false);
-  const [carouselUploaded, setCarouselUploaded] = useState([false, false, false]);
-  
-  // Função para carregar os dados do usuário
-  useEffect(() => {
-    if (user) {
-      // Se o usuário estiver logado, preenche os campos com os dados dele
-      setName(user.name || '');
-      setInstagram(user.instagram || '');
-      setFacebook(user.facebook || '');
-      setWhatsapp(user.whatsapp || '');
-      setPhone(user.phone || '');
-      setAddress(user.address || '');
-      setLogo(user.logo || null);
-      setPixKey(user.pixKey || '');
-      setWebsite(user.website || '');
-      setAbout(user.about || '');
-      setLinkedin(user.linkedin || '');
-      setYoutube(user.youtube || '');
-      setTiktok(user.tiktok || '');
-      setX(user.x || '');
-      setBgColor(user.bgColor || '#ffffff');
-      setLabelBgColor(user.labelBgColor || '#f0f0f0');
-      setFontColor(user.fontColor || '#000000');
-      setBgImage(user.bgImage || null);
-      setCarouselImages(user.carouselImages || [null, null, null]);
-    }
-  }, [user]);
+const [bgImageUploaded, setBgImageUploaded] = useState(false);
+const [carouselUploaded, setCarouselUploaded] = useState([false, false, false]);
+const [markerPosition, setMarkerPosition] = useState([51.505, -0.09]); // Posição inicial do marcador
 
-  // Função para enviar os dados do formulário
-  const handleSubmit = async (e) => {
+  
+  
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (!name || !whatsapp) {
+    if (!name ) {
       setError('Nome e WhatsApp são obrigatórios.');
       return;
     }
@@ -102,44 +78,34 @@ const CardForm = ({ user, onSubmit }) => {
       fontColor,
       bgImage,
       carouselImages,
-      selectedIcon,
+      selectedIcon, // Adicionando ícone selecionado ao objeto de dados
     };
-
-    try {
-      const docRef = await addDoc(collection(db, "users"), cardData); // Armazenando na coleção "users"
-      console.log("Documento gravado com ID: ", docRef.id);
-    } catch (error) {
-      setError("Erro ao salvar os dados no banco de dados.");
-    }
 
     onSubmit(cardData);
     navigate('/preview');
   };
 
-  // Função para atualizar a logo
+ 
   const handleLogoChange = (e) => {
     setLogo(e.target.files[0]);
     setLogoUploaded(true); // Atualiza o estado para indicar que o upload foi feito
   };
 
-  // Função para atualizar a imagem de fundo
   const handleBgImageChange = (e) => {
     setBgImage(e.target.files[0]);
     setBgImageUploaded(true);
   };
 
-  // Função para atualizar as imagens do carrossel
   const handleCarouselImageChange = (index, e) => {
     const updatedImages = [...carouselImages];
     updatedImages[index] = e.target.files[0];
     setCarouselImages(updatedImages);
-
+  
     const updatedStatus = [...carouselUploaded];
     updatedStatus[index] = true;
     setCarouselUploaded(updatedStatus);
   };
 
-  // Função para lidar com a mudança de cor
   const handleColorChange = (color, type) => {
     const rgbaColor = `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`;
     if (type === 'bg') setBgColor(rgbaColor);
@@ -147,10 +113,8 @@ const CardForm = ({ user, onSubmit }) => {
     if (type === 'font') setFontColor(rgbaColor);
   };
 
-  // Função para exibir o seletor de cores
-  const handleColorPickerToggle = (type) => {
-    setShowPicker({ ...showPicker, [type]: !showPicker[type] });
-    setCurrentPicker(type);
+  const handleIconSelect = (icon) => {
+    setSelectedIcon(icon); // Atualiza o ícone selecionado
   };
 
   return (
@@ -159,153 +123,272 @@ const CardForm = ({ user, onSubmit }) => {
         <h1 className="text-center mb-4">Tap Conecte</h1>
         {error && <p className="alert alert-danger">{error}</p>}
 
-        <div className="button-container">
+        <div class="button-container">
           <button className="glow-on-hover" onClick={() => setActiveTab('profile')}>Informações de Perfil</button>
           <button className="glow-on-hover" onClick={() => setActiveTab('social')}>Redes Sociais</button>
-          <button className="glow-on-hover" onClick={() => setActiveTab('slides')}>Personalização</button>
+          <button className="glow-on-hover" onClick={() => setActiveTab('slides')}>Pesonalização</button>
         </div>
 
         <form className="form-container" onSubmit={handleSubmit}>
-          {/* Formulário do perfil */}
           {activeTab === 'profile' && (
             <div>
+              
               <div className="form-group">
-                <label className="label-white">Nome:</label>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                  className="form-control"
-                  placeholder="João Silva"
-                />
-              </div>
-              <div className="form-group">
-                <label className="label-white">Telefone:</label>
-                <input
-                  type="text"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  className="form-control"
-                  placeholder="(XX) XXXX-XXXX"
-                />
-              </div>
-              <div className="form-group">
-                <label className="label-white">Endereço:</label>
-                <input
-                  type="text"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  className="form-control"
-                  placeholder="Rua X, 123"
-                />
-              </div>
-            </div>
-          )}
-
-          {/* Formulário de redes sociais */}
-          {activeTab === 'social' && (
-            <div className="social-container">
-              <div className="form-group">
-                <label className="label-white">Instagram:</label>
-                <input
-                  type="text"
-                  value={instagram}
-                  onChange={(e) => setInstagram(e.target.value)}
-                  className="form-control"
-                  placeholder="https://instagram.com/seunome"
-                />
-              </div>
-              <div className="form-group">
-                <label className="label-white">Facebook:</label>
-                <input
-                  type="text"
-                  value={facebook}
-                  onChange={(e) => setFacebook(e.target.value)}
-                  className="form-control"
-                  placeholder="https://facebook.com/seunome"
-                />
-              </div>
-              <div className="form-group">
-                <label className="label-white">LinkedIn:</label>
-                <input
-                  type="text"
-                  value={linkedin}
-                  onChange={(e) => setLinkedin(e.target.value)}
-                  className="form-control"
-                  placeholder="https://linkedin.com/in/seunome"
-                />
-              </div>
-              <div className="form-group">
-                <label className="label-white">WhatsApp:</label>
-                <input
-                  type="text"
-                  value={whatsapp}
-                  onChange={(e) => setWhatsapp(e.target.value)}
-                  required
-                  className="form-control"
-                  placeholder="(XX) XXXXX-XXXX"
-                />
-              </div>
-            </div>
-          )}
-
-          {/* Personalização */}
-          {activeTab === 'slides' && (
-            <div>
-              <div className="form-group">
-                <label className="label-white">Cor de Fundo:</label>
-                <button type="button" onClick={() => handleColorPickerToggle('bg')} className="btn btn-primary">
-                  Escolher cor de fundo
-                </button>
-                {showPicker.bg && (
-                  <ChromePicker color={bgColor} onChange={(color) => handleColorChange(color, 'bg')} />
-                )}
-              </div>
-              <div className="form-group">
-                <label className="label-white">Cor do Rótulo:</label>
-                <button type="button" onClick={() => handleColorPickerToggle('label')} className="btn btn-primary">
-                  Escolher cor do rótulo
-                </button>
-                {showPicker.label && (
-                  <ChromePicker color={labelBgColor} onChange={(color) => handleColorChange(color, 'label')} />
-                )}
-              </div>
-              <div className="form-group">
-                <label className="label-white">Cor da Fonte:</label>
-                <button type="button" onClick={() => handleColorPickerToggle('font')} className="btn btn-primary">
-                  Escolher cor da fonte
-                </button>
-                {showPicker.font && (
-                  <ChromePicker color={fontColor} onChange={(color) => handleColorChange(color, 'font')} />
-                )}
-              </div>
-
-              {/* Upload de arquivos */}
-              <div className="form-group">
-                <label className="label-white">Logo (Opcional):</label>
-                <input type="file" onChange={handleLogoChange} className="form-control" />
-              </div>
-              <div className="form-group">
-                <label className="label-white">Imagem de Fundo (Opcional):</label>
-                <input type="file" onChange={handleBgImageChange} className="form-control" />
-              </div>
-
-              {/* Carrossel de imagens */}
-              <div className="form-group">
-                <label className="label-white">Carrossel de Imagens (3):</label>
-                {carouselImages.map((image, index) => (
-                  <div key={index} className="carousel-image-upload">
-                    <input type="file" onChange={(e) => handleCarouselImageChange(index, e)} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-
-          <div className="button-container">
-            <button className="glow-on-hover" type="submit">Salvar</button>
+  <label className="label-white">Nome:</label>
+  <input
+    type="text"
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+    required
+    className="form-control"
+    placeholder="João Silva"
+  />
+</div>
+<div className="form-group">
+  <label className="label-white">Telefone:</label>
+  <input
+    type="tel"
+    value={phone}
+    onChange={(e) => setPhone(e.target.value)}
+    className="form-control"
+    placeholder="(99) 99999-9999"
+  />
+</div>
+<div className="form-group">
+<label className="label-white">Endereço:</label>
+<label className="label-white">Endereço:</label>
+  <input
+    type="text"
+    value={address}
+    onChange={(e) => setAddress(e.target.value)}
+    className="form-control"
+    placeholder="Rua A, 123"
+  />
           </div>
+<div className="form-group">
+  <label className="label-white">Sobre:</label>
+  <textarea
+    value={about}
+    onChange={(e) => setAbout(e.target.value)}
+    className="form-control"
+    placeholder="Fale um pouco sobre você"
+  />
+</div>
+
+            </div>
+          )}
+
+{activeTab === 'social' && (
+  <div className="social-container">
+    <div className="social-column">
+      <div className="form-group">
+        <label className="label-white">Instagram:</label>
+        <input
+          type="url"
+          value={instagram}
+          onChange={(e) => setInstagram(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://instagram.com/seuusuario"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">Facebook:</label>
+        <input
+          type="url"
+          value={facebook}
+          onChange={(e) => setFacebook(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://facebook.com/seuusuario"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">WhatsApp:</label>
+        <input
+          type="tel"
+          value={whatsapp}
+          onChange={(e) => setWhatsapp(e.target.value)}
+          required
+          className="form-control"
+          placeholder="Exemplo: +55 11 98765-4321"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">Chave PIX:</label>
+        <input
+          type="text"
+          value={pixKey}
+          onChange={(e) => setPixKey(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: chave@pix.com.br"
+        />
+      </div>
+    </div>
+
+    <div className="social-column">
+      <div className="form-group">
+        <label className="label-white">Website:</label>
+        <input
+          type="url"
+          value={website}
+          onChange={(e) => setWebsite(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://www.seusite.com.br"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">LinkedIn:</label>
+        <input
+          type="url"
+          value={linkedin}
+          onChange={(e) => setLinkedin(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://linkedin.com/in/seunome"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">YouTube:</label>
+        <input
+          type="url"
+          value={youtube}
+          onChange={(e) => setYoutube(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://youtube.com/c/seucanal"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">TikTok:</label>
+        <input
+          type="url"
+          value={tiktok}
+          onChange={(e) => setTiktok(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://tiktok.com/@seunome"
+        />
+      </div>
+      <div className="form-group">
+        <label className="label-white">X (antigo Twitter):</label>
+        <input
+          type="url"
+          value={x}
+          onChange={(e) => setX(e.target.value)}
+          className="form-control"
+          placeholder="Exemplo: https://x.com/seuusuario"
+        />
+      </div>
+    </div>
+  </div>
+)}
+
+
+{activeTab === 'slides' && (
+<div>
+              
+<div className="form-group">
+  <label className="label-white"></label>
+  <label
+    htmlFor="logo-upload"
+    className={`custom-file-upload ${logoUploaded ? "uploaded" : "not-uploaded"}`}>
+    {logoUploaded ? "✅ Logo Carregada!" : "Escolher Logo"}
+  </label>
+  <input type="file" id="logo-upload" onChange={handleLogoChange} className="custom-file-input" />
+</div>
+
+<div className="form-group">
+  <label className="label-white"></label>
+  <label
+    htmlFor="bg-image-upload"
+    className={`custom-file-upload ${bgImageUploaded ? "uploaded" : "not-uploaded"}`}
+  >
+    {bgImageUploaded ? "✅ Imagem Carregada!" : "Escolher Imagem de Fundo"}
+  </label>
+  <input type="file" id="bg-image-upload" onChange={handleBgImageChange} className="custom-file-input" />
+</div>
+
+<div className="form-group">
+  <label className="label-white">Slides:</label>
+  {[0, 1, 2].map((index) => (
+    <div key={index}>
+      <label
+        htmlFor={`carousel-upload-${index}`}
+        className={`custom-file-upload ${carouselUploaded[index] ? "uploaded" : "not-uploaded"}`}
+      >
+        {carouselUploaded[index] ? `✅ Slide ${index + 1} Carregado!` : `Escolher Slide ${index + 1}`}
+      </label>
+      <input
+        type="file"
+        id={`carousel-upload-${index}`}
+        onChange={(e) => handleCarouselImageChange(index, e)}
+        className="custom-file-input"
+      />
+    </div>
+  ))}
+</div>
+
+
+
+
+             
+
+              {/* Seletores de cor */}
+              <div style={{ display: 'flex', gap: '10px' }}>
+                <div>
+                  <button
+                  className="glow-on-hover"
+                    type="button"
+                    onClick={() => {
+                      setShowPicker({ ...showPicker, bg: !showPicker.bg });
+                      setCurrentPicker('bg');
+                    }}
+                  >
+                    Cor de Fundo
+                  </button>
+                  {showPicker.bg && (
+                    <div ref={pickerRef}>
+                      <ChromePicker color={bgColor} onChange={(color) => handleColorChange(color, 'bg')} />
+                    </div>
+                  )}
+                </div>
+
+                <div>
+                  <button
+                  className="glow-on-hover"
+                    type="button"
+                    onClick={() => {
+                      setShowPicker({ ...showPicker, label: !showPicker.label });
+                      setCurrentPicker('label');
+                    }}
+                  >
+                    Cor da caixa de texto
+                  </button>
+                  {showPicker.label && (
+                    <div ref={pickerRef}>
+                      <ChromePicker color={labelBgColor} onChange={(color) => handleColorChange(color, 'label')} />
+                    </div>
+                  )}
+                </div>
+
+                <div>
+                  <button
+                  className="glow-on-hover"
+                    type="button"
+                    onClick={() => {
+                      setShowPicker({ ...showPicker, font: !showPicker.font });
+                      setCurrentPicker('font');
+                    }}
+                  >
+                    Cor da Fonte
+                  </button>
+                  {showPicker.font && (
+                    <div ref={pickerRef}>
+                      <ChromePicker color={fontColor} onChange={(color) => handleColorChange(color, 'font')} />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+
+          <button type="submit" className="btn btn-primary glow-on-hover">Salvar</button>
         </form>
       </div>
     </div>
